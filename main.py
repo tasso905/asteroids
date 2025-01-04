80c88868ef9ae25cf9b1ac01793f720ca3c0b95b
tasso905 <tasso75@hotmail.com>
2025-01-04 14:22:49 -0500

    Added player class & movement.

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-    
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT /2
+    player = Player(x, y)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        
+        player.update(dt)
+
+        screen.fill("black")
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
