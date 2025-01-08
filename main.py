e4c2997975d3dee0a3279e98c1526b270ac2914d
tasso905 <tasso75@hotmail.com>
2025-01-08 17:05:54 -0500

    Shots can now destroy asteroids or split them.

--- main.py
+++ main.py
@@ -42,8 +42,8 @@ def main():
                 sys.exit()
             for shot in shots:
                 if asteroid.collides_with(shot):
-                    asteroid.kill()
                     shot.kill()
+                    asteroid.split()
 
         screen.fill("black")
 
